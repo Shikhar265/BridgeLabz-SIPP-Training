309c3ed89e0ae66f2bda94da1629d42aa6dc7250
Shikhar <gshikhar1383@gmail.com>
2025-06-23 13:37:25 +0530

    Deleted the extra files

--- Java_Fundamentals/Harry_age.java
+++
@@ -1,9 +0,0 @@
-class Harry
-{
-    public static void main(String args[])
-    {
-	int b_year=2000, c_year=2024;
-	int age=c_year-b_year;
-	System.out.print("Harry's age in 2024 is "+age);
-    }
-}
